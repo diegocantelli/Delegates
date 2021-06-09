818087ce2ec7247a8bd9938c7f8c6ca97c5de3ca
Diego Cantelli <diegocantelli19@gmail.com>
2021-06-09 19:43:38 -0300

    Initial commit

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Delegates
+{
+    // este delegate pode ser apontado para qualquer função que tenha a mesma assinatura 
+    public delegate void HelloFunctionDelegate(string message);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
