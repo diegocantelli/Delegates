a601d47e533e16938a21a2433818b119f9dfd3ae
Diego Cantelli <diegocantelli19@gmail.com>
2021-06-10 19:21:19 -0300

    Criando o cenário para usar delegates

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Delegates
 {
@@ -11,14 +12,40 @@ namespace Delegates
         static void Main(string[] args)
         {
             // o método Hello é passado como parâmetro pois possui a mesma assinatura entre a delegato e o método
-            HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
-            del("Chamando a função hello");
-            Console.WriteLine("Hello World!");
+            //HelloFunctionDelegate del = new HelloFunctionDelegate(Hello);
+            //del("Chamando a função hello");
+            //Console.WriteLine("Hello World!");
+
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee { ID = 101, Name = "name 1", Salary = 5000, Experience = 5 });
+            empList.Add(new Employee { ID = 101, Name = "name 2", Salary = 4000, Experience = 4 });
+            empList.Add(new Employee { ID = 101, Name = "name 3", Salary = 6000, Experience = 6 });
+            empList.Add(new Employee { ID = 101, Name = "name 4", Salary = 3000, Experience = 3 });
+            Employee.PromoteEmployee(empList);
         }
 
         public static void Hello(string message)
         {
             Console.WriteLine(message);
+        }
+    }
+
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+        public static void PromoteEmployee(List<Employee> employeeList)
+        {
+            foreach (var item in employeeList)
+            {
+                if(item.Experience > 5)
+                {
+                    Console.WriteLine(item.Name + " promoted");
+                }
+            }
         }
     }
 }
